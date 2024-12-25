a80cf17b899c81c3cfbab82c8e5eed2393453455
Rajeshsharma-123 <079bct060@ioepc.edu.np>
2024-12-25 14:23:51 +0545

    another commit

--- flask1/controller/user_controller.py
+++ flask1/controller/user_controller.py
@@ -1,4 +1,6 @@
 from app import app
+from model.user_model import user_modal
+obj = user_modal()
 @app.route("/user/signup")
-def signup():
-    return "this is signup operation"
+def user_signup_controller():
+    return obj.user_signup_model()
---
+++ flask1/model/user_model.py
@@ -0,0 +1,3 @@
+class user_modal():
+    def user_signup_model(self):
+        return "This is user_signup_model"
