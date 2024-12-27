6e6eee1f1719b7ac348a6d1a979b55191cacaa86
Rajeshsharma-123 <079bct060@ioepc.edu.np>
2024-12-27 21:30:06 +0545

    update query completed

--- flask1/controller/user_controller.py
+++ flask1/controller/user_controller.py
@@ -1,6 +1,17 @@
 from app import app
 from model.user_model import user_modal
+from flask import request
 obj = user_modal()
+
 @app.route("/user/getall")
 def user_getall_controller():
     return obj.user_getall_model()
+
+@app.route("/user/addone")
+def user_addone_controller():
+    return obj.user_addone_model(request.form)
+    
+@app.route("/user/update", methods=["PUT"])
+def user_update_controller():
+    return obj.user_update_model(request.form)
+    
--- flask1/model/user_model.py
+++ flask1/model/user_model.py
@@ -4,6 +4,7 @@ class user_modal():
     def __init__(self):
         try:
             self.con=mysql.connector.connect(host="localhost",user="root",password="Rsharma#123",database="flask_tutorail")
+            self.con.autocommit=True
             self.cur=self.con.cursor(dictionary=True)
             print("Connection Successfull")
         except:
@@ -22,4 +23,16 @@ class user_modal():
         else:
             return "No Data found"
         
+    def user_addone_model(self, data):
+        
+        self.cur.execute(f"INSERT INTO users(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')")
+        return "User Created Successfully"                             
+       
+    def user_update_model(self, data):
+        self.cur.execute(f"UPDATE users SET name='{data['name']}', email='{data['email']}' , phone='{data['phone']}' , role='{data['role']}', password='{data['password']}' WHERE idusers={data['idusers']} ")
+        if self.cur.rowcount>0:
+            return "User Updated Successfully"
+        else:
+            return "Nothing to Update"                            
+       
         
