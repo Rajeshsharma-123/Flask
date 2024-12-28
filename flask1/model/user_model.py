6cfba92ae6751918352c6a8d63c0ec38a2ccf4ab
Rajeshsharma-123 <079bct060@ioepc.edu.np>
2024-12-28 15:55:54 +0545

    update with make_response() function and response codes

--- flask1/model/user_model.py
+++ flask1/model/user_model.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import json
-from flask import jsonify
+from flask import make_response
 class user_modal():
     def __init__(self):
         try:
@@ -20,29 +20,29 @@ class user_modal():
         self.cur.execute("SELECT * FROM users ")
         result=self.cur.fetchall()
         if len(result)>0:
-            return {"payload":result}
+            return make_response({"payload":result},200)
             # return json.dumps(result)
         else:
-            return {"message":"No Data found"}
+            return make_response({"message":"No Data found"},204)
         
     def user_addone_model(self, data):
         
         self.cur.execute(f"INSERT INTO users(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')")
-        return jsonify({"message":"User Created Successfully"}), 201                             
+        return make_response({"message":"User Created Successfully"},201)                             
        
     def user_update_model(self, data):
         self.cur.execute(f"UPDATE users SET name='{data['name']}', email='{data['email']}' , phone='{data['phone']}' , role='{data['role']}', password='{data['password']}' WHERE idusers={data['idusers']} ")
         if self.cur.rowcount>0:
-            return {"message":"User Updated Successfully"}
+            return make_response({"message":"User Updated Successfully"},201)
         else:
-            return {"message":"Nothing to Update"}                            
+            return make_response({"message":"Nothing to Update"},202)                            
        
         
     def user_delete_model(self, idusers):
         self.cur.execute(f"DELETE FROM users WHERE idusers={idusers} ")
         if self.cur.rowcount>0:
-            return {"message":"User Deleted Successfully"}
+            return make_response({"message":"User Deleted Successfully"},200)
         else:
-            return {"message":"Nothing to Delete"}                            
+            return make_response({"message":"Nothing to Delete"},202)                            
        
         
